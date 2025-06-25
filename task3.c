cb2145cb585c1120fa5c77887a83f94ebb52b1ac
Sheetal Dave <sheetaldave0312@gmail.com>
2025-06-25 16:19:07 +0530

    Update task3.c

--- task3.c
+++ task3.c
@@ -1,43 +1,29 @@
 #include <stdio.h>
 
-void main()
-{
-    // size input
-    int size;
-    printf("Enter size of array:");
+void main() {
+    // size
+    int size, sum = 0;
+    printf("Enter size of array: ");
     scanf("%d", &size);
 
     // array input
     int a[size];
-    for (int i = 0; i < size; i++)
-    {
-        printf("Enter %d value\n", i + 1);
+    printf("Enter %d values:\n", size);
+    for (int i = 0; i < size; i++) {
         scanf("%d", &a[i]);
+        sum += a[i];
     }
-
-    // printf  actual  array
-    printf("Actual array\n");
+    
+// actual array
+    printf("Actual array:\n");
     for (int i = 0; i < size; i++)
-    {
+        printf("%d\t", a[i]);
 
-        printf("%d \t", a[i]);
-    }
-
-    // printf  reverse  array
-    int b[size];
-    printf("\nReverse array\n");
+    // reverse array;
+    printf("\nReverse array:\n");
     for (int i = size - 1; i >= 0; i--)
-    {
-        b[i] = a[i];
-        printf("%d\t", b[i]);
-    }
+        printf("%d\t", a[i]);
 
-    // sum
-    int sum = 0;
-    for (int i = 0; i < size; i++)
-    {
-
-        sum = a[i] + sum;
-    }
-    printf("\n\n sum of array is %d", sum);
+    printf("\nSum of array: %d\n", sum);
+    
 }
